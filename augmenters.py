c6f8e8cec11530ffb47f169349be34508c9983ac
aleju <kontakt@ajung.name>
2016-11-25 22:23:33 +0100

    Add grayscale augmenter

--- augmenters.py
+++ augmenters.py
@@ -10,6 +10,7 @@ from skimage import transform as tf
 import itertools
 import imgaug as ia
 from parameters import StochasticParameter, Deterministic, Binomial, Choice, DiscreteUniform, Normal, Uniform
+import cv2
 
 try:
     xrange
@@ -852,6 +853,143 @@ class Flipud(Augmenter):
 
     def get_parameters(self):
         return [self.p]
+
+# Note: Not clear whether this class will be kept (for anything aside from grayscale)
+# other colorspaces dont really make sense and they also might not work correctly
+# due to having no clearly limited range (like 0-255 or 0-1)
+class ChangeColorspace(Augmenter):
+    RGB = "RGB"
+    BGR = "BGR"
+    GRAY = "GRAY"
+    CIE = "CIE"
+    YCrCb = "YCrCb"
+    HSV = "HSV"
+    HLS = "HLS"
+    Lab = "Lab"
+    Luv = "Luv"
+    COLORSPACES = set([
+        RGB,
+        BGR,
+        GRAY,
+        CIE,
+        YCrCb,
+        HSV,
+        HLS,
+        Lab,
+        Luv
+    ])
+    CV_VARS = {
+        # RGB
+        #"RGB2RGB": cv2.COLOR_RGB2RGB,
+        "RGB2BGR": cv2.COLOR_RGB2BGR,
+        "RGB2GRAY": cv2.COLOR_RGB2GRAY,
+        "RGB2CIE": cv2.COLOR_RGB2XYZ,
+        "RGB2YCrCb": cv2.COLOR_RGB2YCR_CB,
+        "RGB2HSV": cv2.COLOR_RGB2HSV,
+        "RGB2HLS": cv2.COLOR_RGB2HLS,
+        "RGB2LAB": cv2.COLOR_RGB2LAB,
+        "RGB2LUV": cv2.COLOR_RGB2LUV,
+        # BGR
+        "BGR2RGB": cv2.COLOR_BGR2RGB,
+        #"BGR2BGR": cv2.COLOR_BGR2BGR,
+        "BGR2GRAY": cv2.COLOR_BGR2GRAY,
+        "BGR2CIE": cv2.COLOR_BGR2XYZ,
+        "BGR2YCrCb": cv2.COLOR_BGR2YCR_CB,
+        "BGR2HSV": cv2.COLOR_BGR2HSV,
+        "BGR2HLS": cv2.COLOR_BGR2HLS,
+        "BGR2LAB": cv2.COLOR_BGR2LAB,
+        "BGR2LUV": cv2.COLOR_BGR2LUV
+    }
+
+    def __init__(self, to_colorspace, alpha, from_colorspace="RGB", name=None, deterministic=False, random_state=None):
+        Augmenter.__init__(self, name=name, deterministic=deterministic, random_state=random_state)
+
+        if ia.is_single_number(alpha):
+            self.alpha = Deterministic(alpha)
+        elif ia.is_iterable(alpha):
+            assert len(alpha) == 2, "Expected tuple/list with 2 entries, got %d entries." % (str(len(alpha)),)
+            self.alpha = Uniform(alpha[0], alpha[1])
+        elif isinstance(p, StochasticParameter):
+            self.alpha = alpha
+        else:
+            raise Exception("Expected alpha to be int or float or tuple/list of ints/floats or StochasticParameter, got %s." % (type(alpha),))
+
+        if ia.is_string(to_colorspace):
+            assert to_colorspace in ChangeColorspace.COLORSPACES
+            self.to_colorspace = Deterministic(to_colorspace)
+        elif ia.is_iterable(to_colorspace):
+            assert all([ia.is_string(colorspace) for colorspace in to_colorspace])
+            assert all([(colorspace in ChangeColorspace.COLORSPACES) for colorspace in to_colorspace])
+            self.to_colorspace = Choice(to_colorspace)
+        elif isinstance(to_colorspace, StochasticParameter):
+            self.to_colorspace = to_colorspace
+        else:
+            raise Exception("Expected to_colorspace to be string, list of strings or StochasticParameter, got %s." % (type(to_colorspace),))
+
+        self.from_colorspace = from_colorspace
+        assert self.from_colorspace in ChangeColorspace.COLORSPACES
+        assert from_colorspace != ChangeColorspace.GRAY
+
+    def _augment_images(self, images, random_state, parents, hooks):
+        result = images
+        nb_images = len(images)
+        alphas = self.alpha.draw_samples((nb_images,), random_state=ia.copy_random_state(random_state))
+        to_colorspaces = self.to_colorspace.draw_samples((nb_images,), random_state=ia.copy_random_state(random_state))
+        for i in xrange(nb_images):
+            alpha = alphas[i]
+            to_colorspace = to_colorspaces[i]
+            image = images[i]
+
+            assert 0.0 <= alpha <= 1.0
+            assert to_colorspace in ChangeColorspace.COLORSPACES
+
+            if alpha == 0 or self.from_colorspace == to_colorspace:
+                pass # no change necessary
+            else:
+                # some colorspaces here should use image/255.0 according to the docs,
+                # but at least for conversion to grayscale that results in errors,
+                # ie uint8 is expected
+
+                if self.from_colorspace in [ChangeColorspace.RGB, ChangeColorspace.BGR]:
+                    from_to_var_name = "%s2%s" % (self.from_colorspace, to_colorspace)
+                    from_to_var = ChangeColorspace.CV_VARS[from_to_var_name]
+                    img_to_cs = cv2.cvtColor(image, from_to_var)
+                else:
+                    # convert to RGB
+                    from_to_var_name = "%s2%s" % (self.from_colorspace, ChangeColorspace.RGB)
+                    from_to_var = ChangeColorspace.CV_VARS[from_to_var_name]
+                    img_rgb = cv2.cvtColor(image, from_to_var)
+
+                    # convert from RGB to desired target colorspace
+                    from_to_var_name = "%s2%s" % (ChangeColorspace.RGB, to_colorspace)
+                    from_to_var = ChangeColorspace.CV_VARS[from_to_var_name]
+                    img_to_cs = cv2.cvtColor(img_rgb, from_to_var)
+
+                # this will break colorspaces that have values outside 0-255 or 0.0-1.0
+                if ia.is_integer_array(img_to_cs):
+                    img_to_cs = np.clip(img_to_cs, 0, 255).astype(np.uint8)
+                else:
+                    img_to_cs = np.clip(img_to_cs * 255, 0, 255).astype(np.uint8)
+
+                if len(img_to_cs.shape) == 2:
+                    img_to_cs = img_to_cs[:, :, np.newaxis]
+                    img_to_cs = np.tile(img_to_cs, (1, 1, 3))
+
+                if alpha == 1:
+                    result[i] = img_to_cs
+                else:
+                    result[i] = (alpha * img_to_cs + (1 - alpha) * image).astype(np.uint8)
+
+        return images
+
+    def _augment_keypoints(self, keypoints_on_images, random_state, parents, hooks):
+        return keypoints_on_images
+
+    def get_parameters(self):
+        return [self.alpha, self.to_colorspace]
+
+def Grayscale(alpha=0, from_colorspace="RGB", name=None, deterministic=False, random_state=None):
+    return ChangeColorspace(to_colorspace=ChangeColorspace.GRAY, alpha=alpha, from_colorspace=from_colorspace, name=name, deterministic=deterministic, random_state=random_state)
 
 class GaussianBlur(Augmenter):
     def __init__(self, sigma=0, name=None, deterministic=False, random_state=None):
