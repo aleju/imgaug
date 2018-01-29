53219d8e07b6c451ad517b0b8079d62ded5cfe4d
aleju <kontakt@ajung.name>
2018-01-29 20:30:07 +0100

    Add test for AddElementwise

--- tests/test.py
+++ tests/test.py
@@ -1487,6 +1487,111 @@ def test_Add():
     assert nb_changed_aug >= int(nb_iterations * 0.7)
     assert nb_changed_aug_det == 0
 
+def test_AddElementwise():
+    reseed()
+
+    base_img = np.ones((3, 3, 1), dtype=np.uint8) * 100
+    images = np.array([base_img])
+    images_list = [base_img]
+    keypoints = [ia.KeypointsOnImage([ia.Keypoint(x=0, y=0), ia.Keypoint(x=1, y=1),
+                                      ia.Keypoint(x=2, y=2)], shape=base_img.shape)]
+
+    # no add, shouldnt change anything
+    aug = iaa.AddElementwise(value=0)
+    aug_det = aug.to_deterministic()
+
+    observed = aug.augment_images(images)
+    expected = images
+    assert np.array_equal(observed, expected)
+
+    observed = aug.augment_images(images_list)
+    expected = images_list
+    assert array_equal_lists(observed, expected)
+
+    observed = aug_det.augment_images(images)
+    expected = images
+    assert np.array_equal(observed, expected)
+
+    observed = aug_det.augment_images(images_list)
+    expected = images_list
+    assert array_equal_lists(observed, expected)
+
+    # add > 0
+    aug = iaa.AddElementwise(value=1)
+    aug_det = aug.to_deterministic()
+
+    observed = aug.augment_images(images)
+    expected = images + 1
+    assert np.array_equal(observed, expected)
+
+    observed = aug.augment_images(images_list)
+    expected = [images_list[0] + 1]
+    assert array_equal_lists(observed, expected)
+
+    observed = aug_det.augment_images(images)
+    expected = images + 1
+    assert np.array_equal(observed, expected)
+
+    observed = aug_det.augment_images(images_list)
+    expected = [images_list + 1]
+    assert array_equal_lists(observed, expected)
+
+    # add < 0
+    aug = iaa.AddElementwise(value=-1)
+    aug_det = aug.to_deterministic()
+
+    observed = aug.augment_images(images)
+    expected = images - 1
+    assert np.array_equal(observed, expected)
+
+    observed = aug.augment_images(images_list)
+    expected = [images_list[0] - 1]
+    assert array_equal_lists(observed, expected)
+
+    observed = aug_det.augment_images(images)
+    expected = images - 1
+    assert np.array_equal(observed, expected)
+
+    observed = aug_det.augment_images(images_list)
+    expected = [images_list - 1]
+    assert array_equal_lists(observed, expected)
+
+    # keypoints shouldnt be changed
+    aug = iaa.AddElementwise(value=1)
+    aug_det = iaa.AddElementwise(value=1).to_deterministic()
+    observed = aug.augment_keypoints(keypoints)
+    expected = keypoints
+    assert keypoints_equal(observed, expected)
+
+    observed = aug_det.augment_keypoints(keypoints)
+    expected = keypoints
+    assert keypoints_equal(observed, expected)
+
+    # varying values
+    aug = iaa.AddElementwies(value=(0, 10))
+    aug_det = aug.to_deterministic()
+
+    last_aug = None
+    last_aug_det = None
+    nb_changed_aug = 0
+    nb_changed_aug_det = 0
+    nb_iterations = 1000
+    for i in sm.xrange(nb_iterations):
+        observed_aug = aug.augment_images(images)
+        observed_aug_det = aug_det.augment_images(images)
+        if i == 0:
+            last_aug = observed_aug
+            last_aug_det = observed_aug_det
+        else:
+            if not np.array_equal(observed_aug, last_aug):
+                nb_changed_aug += 1
+            if not np.array_equal(observed_aug_det, last_aug_det):
+                nb_changed_aug_det += 1
+            last_aug = observed_aug
+            last_aug_det = observed_aug_det
+    assert nb_changed_aug >= int(nb_iterations * 0.7)
+    assert nb_changed_aug_det == 0
+
 def test_Affine():
     reseed()
 
