e109f273847080544690d17c10c15476d24a0bd9
aleju <kontakt@ajung.name>
2018-05-13 17:22:49 +0200

    Add test for Superpixels

--- tests/test.py
+++ tests/test.py
@@ -103,7 +103,7 @@ def main():
     # TODO FrequencyNoiseAlpha
 
     # segmentation
-    # TODO superpixels
+    test_Superpixels()
 
     # size
     # TODO Scale
@@ -689,6 +689,62 @@ def test_Alpha():
     for nb_samples in hist:
         density = nb_samples / nb_iterations
         assert density_expected - density_tolerance < density < density_expected + density_tolerance
+
+
+def test_Superpixels():
+    reseed()
+
+    def _array_equals_tolerant(a, b, tolerance):
+        diff = np.abs(a.astype(np.int32) - b.astype(np.int32))
+        return np.all(diff <= tolerance)
+
+    base_img = [
+        [255, 255, 255, 0, 0, 0],
+        [255, 235, 255, 0, 20, 0],
+        [250, 250, 250, 5, 5, 5]
+    ]
+    base_img = np.tile(np.array(base_img, dtype=np.uint8)[..., np.newaxis], (1, 1, 3))
+
+    base_img_superpixels = [
+        [251, 251, 251, 4, 4, 4],
+        [251, 251, 251, 4, 4, 4],
+        [251, 251, 251, 4, 4, 4]
+    ]
+    base_img_superpixels = np.tile(np.array(base_img_superpixels, dtype=np.uint8)[..., np.newaxis], (1, 1, 3))
+
+    base_img_superpixels_left = np.copy(base_img_superpixels)
+    base_img_superpixels_left[:, 3:, :] = base_img[:, 3:, :]
+
+    base_img_superpixels_right = np.copy(base_img_superpixels)
+    base_img_superpixels_right[:, :3, :] = base_img[:, :3, :]
+
+    aug = iaa.Superpixels(p_replace=0, n_segments=2)
+    observed = aug.augment_image(base_img)
+    expected = base_img
+    assert np.allclose(observed, expected)
+
+    aug = iaa.Superpixels(p_replace=1.0, n_segments=2)
+    observed = aug.augment_image(base_img)
+    expected = base_img_superpixels
+    assert _array_equals_tolerant(observed, expected, 2)
+
+    aug = iaa.Superpixels(p_replace=0.5, n_segments=2)
+    seen = {"none": False, "left": False, "right": False, "both": False}
+    for _ in sm.xrange(100):
+        observed = aug.augment_image(base_img)
+        if _array_equals_tolerant(observed, base_img, 2):
+            seen["none"] = True
+        elif _array_equals_tolerant(observed, base_img_superpixels_left, 2):
+            seen["left"] = True
+        elif _array_equals_tolerant(observed, base_img_superpixels_right, 2):
+            seen["right"] = True
+        elif _array_equals_tolerant(observed, base_img_superpixels, 2):
+            seen["both"] = True
+        else:
+            raise Exception("Generated superpixels image does not match any expected image.")
+        if all(seen.values()):
+            break
+    assert all(seen.values())
 
 
 def test_Pad():
