899512bf220a5fec439747091ddf16cc090ed55d
aleju <kontakt@ajung.name>
2018-05-15 16:59:47 +0200

    Add test for MedianBlur

--- tests/test.py
+++ tests/test.py
@@ -52,7 +52,7 @@ def main():
     # blur
     test_GaussianBlur()
     test_AverageBlur()
-    # TODO MedianBlur
+    test_MedianBlur()
     # TODO BilateralBlur
 
     # color
@@ -1680,8 +1680,6 @@ def test_AverageBlur():
     base_img[5, 4, 0] = 100
     base_img[5, 6, 0] = 100
 
-    blur0 = np.copy(base_img)
-
     blur3x3 = np.copy(base_img)
     blur3x3 = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -1776,7 +1774,7 @@ def test_AverageBlur():
         elif np.array_equal(observed, blur5x5):
             nb_seen[2] += 1
         else:
-            raise Exception("Unexpected result in AverageBlur@1")
+            raise Exception("Unexpected result in AverageBlur@2")
     p_seen = [v/nb_iterations for v in nb_seen]
     assert 0.23 <= p_seen[0] <= 0.43
     assert 0.23 <= p_seen[1] <= 0.43
@@ -1793,7 +1791,7 @@ def test_AverageBlur():
         elif np.array_equal(observed, blur5x5):
             nb_seen[1] += 1
         else:
-            raise Exception("Unexpected result in AverageBlur@2")
+            raise Exception("Unexpected result in AverageBlur@3")
     p_seen = [v/nb_iterations for v in nb_seen]
     assert 0.4 <= p_seen[0] <= 0.6
     assert 0.4 <= p_seen[1] <= 0.6
@@ -1815,8 +1813,8 @@ def test_AverageBlur():
     nb_seen = dict([(key, 0) for key, val in possible.items()])
     for i in sm.xrange(nb_iterations):
         observed = aug.augment_image(base_img)
-        for key, imgaug in possible.items():
-            if np.array_equal(observed, imgaug):
+        for key, img_aug in possible.items():
+            if np.array_equal(observed, img_aug):
                 nb_seen[key] += 1
     # dont check sum here, because 0xX and Xx0 are all the same, i.e. much
     # higher sum than nb_iterations
@@ -1824,6 +1822,102 @@ def test_AverageBlur():
 
     # keypoints shouldnt be changed
     aug = iaa.AverageBlur(k=3)
+    aug_det = aug.to_deterministic()
+    observed = aug.augment_keypoints(keypoints)
+    expected = keypoints
+    assert keypoints_equal(observed, expected)
+
+    observed = aug_det.augment_keypoints(keypoints)
+    expected = keypoints
+    assert keypoints_equal(observed, expected)
+
+
+def test_MedianBlur():
+    reseed()
+
+    base_img = np.zeros((11, 11, 1), dtype=np.uint8)
+    base_img[3:8, 3:8, 0] = 1
+    base_img[4:7, 4:7, 0] = 2
+    base_img[5:6, 5:6, 0] = 3
+
+    blur3x3 = np.zeros_like(base_img)
+    blur3x3[3:8, 3:8, 0] = 1
+    blur3x3[4:7, 4:7, 0] = 2
+    blur3x3[4, 4, 0] = 1
+    blur3x3[4, 6, 0] = 1
+    blur3x3[6, 4, 0] = 1
+    blur3x3[6, 6, 0] = 1
+    blur3x3[3, 3, 0] = 0
+    blur3x3[3, 7, 0] = 0
+    blur3x3[7, 3, 0] = 0
+    blur3x3[7, 7, 0] = 0
+
+    blur5x5 = np.copy(blur3x3)
+    blur5x5[4, 3, 0] = 0
+    blur5x5[3, 4, 0] = 0
+    blur5x5[6, 3, 0] = 0
+    blur5x5[7, 4, 0] = 0
+    blur5x5[4, 7, 0] = 0
+    blur5x5[3, 6, 0] = 0
+    blur5x5[6, 7, 0] = 0
+    blur5x5[7, 6, 0] = 0
+    blur5x5[blur5x5 > 1] = 1
+    #blur5x5 = np.zeros_like(base_img)
+    #blur5x5[2:9, 2:9, 0] = 1
+    #blur5x5[3:8, 3:8, 0] = 1
+    #blur5x5[4:7, 4:7, 0] = 1
+    #blur5x5[5:6, 5:6, 0] = 1
+
+    keypoints = [ia.KeypointsOnImage([ia.Keypoint(x=0, y=0), ia.Keypoint(x=1, y=1),
+                                      ia.Keypoint(x=2, y=2)], shape=base_img.shape)]
+
+    # no blur, shouldnt change anything
+    aug = iaa.MedianBlur(k=1)
+    observed = aug.augment_image(base_img)
+    assert np.array_equal(observed, base_img)
+
+    # k=3
+    aug = iaa.MedianBlur(k=3)
+    observed = aug.augment_image(base_img)
+    assert np.array_equal(observed, blur3x3)
+
+    # k=5
+    aug = iaa.MedianBlur(k=5)
+    observed = aug.augment_image(base_img)
+    assert np.array_equal(observed, blur5x5)
+
+    # k as (3, 5)
+    aug = iaa.MedianBlur(k=(3, 5))
+    seen = [False, False]
+    for i in sm.xrange(100):
+        observed = aug.augment_image(base_img)
+        if np.array_equal(observed, blur3x3):
+            seen[0] = True
+        elif np.array_equal(observed, blur5x5):
+            seen[1] = True
+        else:
+            raise Exception("Unexpected result in MedianBlur@1")
+        if all(seen):
+            break
+    assert all(seen)
+
+    # k as stochastic parameter
+    aug = iaa.MedianBlur(k=iap.Choice([3, 5]))
+    seen = [False, False]
+    for i in sm.xrange(100):
+        observed = aug.augment_image(base_img)
+        if np.array_equal(observed, blur3x3):
+            seen[0] += True
+        elif np.array_equal(observed, blur5x5):
+            seen[1] += True
+        else:
+            raise Exception("Unexpected result in MedianBlur@2")
+        if all(seen):
+            break
+    assert all(seen)
+
+    # keypoints shouldnt be changed
+    aug = iaa.MedianBlur(k=3)
     aug_det = aug.to_deterministic()
     observed = aug.augment_keypoints(keypoints)
     expected = keypoints
