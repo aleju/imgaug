63ed304716711a31c92ecdbb7efb55bdf0fd656c
aleju <kontakt@ajung.name>
2018-08-03 19:34:03 +0200

    Add checks for heatmaps and segmentation maps

---
+++ checks/check_heatmaps.py
@@ -0,0 +1,162 @@
+from __future__ import print_function
+import imgaug as ia
+from imgaug import augmenters as iaa
+from scipy import misc
+import numpy as np
+
+def main():
+    quokka = ia.quokka(size=0.5)
+    h, w = quokka.shape[0:2]
+    heatmap = np.zeros((h, w), dtype=np.float32)
+    heatmap[70:120, 90:150] = 0.1
+    heatmap[30:70, 50:65] = 0.5
+    heatmap[20:50, 55:85] = 1.0
+    heatmap[120:140, 0:20] = 0.75
+
+    heatmaps = ia.HeatmapsOnImage(heatmap[..., np.newaxis], quokka.shape)
+
+    print("Affine...")
+    aug = iaa.Affine(translate_px={"x": 20}, mode="constant", cval=128)
+    quokka_aug = aug.augment_image(quokka)
+    heatmaps_aug = aug.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+    print("Affine with mode=edge...")
+    aug = iaa.Affine(translate_px={"x": 20}, mode="edge")
+    quokka_aug = aug.augment_image(quokka)
+    heatmaps_aug = aug.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+    print("PiecewiseAffine...")
+    aug = iaa.PiecewiseAffine(scale=0.04)
+    aug_det = aug.to_deterministic()
+    quokka_aug = aug_det.augment_image(quokka)
+    heatmaps_aug = aug_det.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+    print("PerspectiveTransform...")
+    aug = iaa.PerspectiveTransform(scale=0.04)
+    aug_det = aug.to_deterministic()
+    quokka_aug = aug_det.augment_image(quokka)
+    heatmaps_aug = aug_det.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+    print("ElasticTransformation alpha=3, sig=0.5...")
+    aug = iaa.ElasticTransformation(alpha=3.0, sigma=0.5)
+    aug_det = aug.to_deterministic()
+    quokka_aug = aug_det.augment_image(quokka)
+    heatmaps_aug = aug_det.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+    print("ElasticTransformation alpha=10, sig=3...")
+    aug = iaa.ElasticTransformation(alpha=10.0, sigma=3.0)
+    aug_det = aug.to_deterministic()
+    quokka_aug = aug_det.augment_image(quokka)
+    heatmaps_aug = aug_det.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+    print("CopAndPad mode=constant...")
+    aug = iaa.CropAndPad(px=(-10, 10, 15, -15), pad_mode="constant", pad_cval=128)
+    aug_det = aug.to_deterministic()
+    quokka_aug = aug_det.augment_image(quokka)
+    heatmaps_aug = aug_det.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+    print("CropAndPad mode=edge...")
+    aug = iaa.CropAndPad(px=(-10, 10, 15, -15), pad_mode="edge")
+    aug_det = aug.to_deterministic()
+    quokka_aug = aug_det.augment_image(quokka)
+    heatmaps_aug = aug_det.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+    print("Scale...")
+    aug = iaa.Scale(0.5, interpolation="nearest")
+    aug_det = aug.to_deterministic()
+    quokka_aug = aug_det.augment_image(quokka)
+    heatmaps_aug = aug_det.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(ia.draw_grid([heatmaps_drawn[0], heatmaps_aug_drawn[0]], cols=2))
+
+    print("Alpha...")
+    aug = iaa.Alpha(0.7, iaa.Affine(rotate=20))
+    aug_det = aug.to_deterministic()
+    quokka_aug = aug_det.augment_image(quokka)
+    heatmaps_aug = aug_det.augment_heatmaps([heatmaps])[0]
+    heatmaps_drawn = heatmaps.draw_on_image(quokka)
+    heatmaps_aug_drawn = heatmaps_aug.draw_on_image(quokka_aug)
+
+    misc.imshow(
+        np.hstack([
+            heatmaps_drawn[0],
+            heatmaps_aug_drawn[0]
+        ])
+    )
+
+if __name__ == "__main__":
+    main()
