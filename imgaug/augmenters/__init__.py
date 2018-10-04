4ac0ccb010e8a2d405bb502136bf7f603819998e
aleju <kontakt@ajung.name>
2018-10-04 21:52:26 +0200

    Add contrast adjustment augmenters
    This commit adds four new augmenters for
    contrast adjustments, with three of these
    being based on scikit-image's functions.
    + Add GammaContrast: Changes contrast based
      on exponentiation of pixel intensities.
    + Add SigmoidContrast: Changes contrast
      based on applying a sigmoid function to
      pixel intensities.
    + Add LogContrast: Changes contrast based
      on applying a logarithn to pixel
      intensities.
    + Add LinearContrast: Changes contrast
      based on linearly stretching/contracting
      the difference between pixel intensities
      and 128. Same as ContrastNormalization
      and will replace that augmenter in the
      future.

--- imgaug/augmenters/__init__.py
+++ imgaug/augmenters/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from imgaug.augmenters.arithmetic import *
 from imgaug.augmenters.blur import *
 from imgaug.augmenters.color import *
+from imgaug.augmenters.contrast import *
 from imgaug.augmenters.convolutional import *
 from imgaug.augmenters.flip import *
 from imgaug.augmenters.geometric import *
