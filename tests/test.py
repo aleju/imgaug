d52ed184a4b8a933847f52746c31cff01a35b934
aleju <kontakt@ajung.name>
2018-10-05 10:46:49 +0200

    Add test for SigmoidContrast

--- tests/test.py
+++ tests/test.py
@@ -29,6 +29,7 @@ import time
 import scipy
 import copy
 import warnings
+import itertools
 
 #from nose.plugins.attrib import attr
 
@@ -148,9 +149,8 @@ def main():
     test_Grayscale()
 
     # contrast
-    # TODO GammaContrast
     test_GammaContrast()
-    # TODO SigmoidContrast
+    test_SigmoidContrast()
     # TODO LogContrast
     # TODO LinearContrast
 
@@ -5774,7 +5774,6 @@ def test_GammaContrast():
             aug = iaa.GammaContrast(gamma=iap.Deterministic(gamma), per_channel=per_channel)
             img_aug = aug.augment_image(img)
             img3d_aug = aug.augment_image(img3d)
-            sk = skimage.exposure.adjust_gamma(img, gamma=gamma)
             assert img_aug.dtype.type == np.uint8
             assert img3d_aug.dtype.type == np.uint8
             assert np.array_equal(img_aug, skimage.exposure.adjust_gamma(img, gamma=gamma))
@@ -5817,6 +5816,80 @@ def test_GammaContrast():
     # check that heatmaps are not changed
     heatmaps = ia.HeatmapsOnImage(np.zeros((3, 3, 1), dtype=np.float32) + 0.5, shape=(3, 3, 3))
     heatmaps_aug = iaa.GammaContrast(gamma=2).augment_heatmaps([heatmaps])[0]
+    assert np.allclose(heatmaps.arr_0to1, heatmaps_aug.arr_0to1)
+
+
+def test_SigmoidContrast():
+    reseed()
+
+    img = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    img = np.uint8(img)
+    img3d = np.tile(img[:, :, np.newaxis], (1, 1, 3))
+
+    # check basic functionality with per_chanenl on/off (makes no difference due to deterministic
+    # parameters)
+    for per_channel in [False, 0, 0.0, True, 1, 1.0]:
+        for gain, cutoff in itertools.product([5, 10], [0.25, 0.75]):
+            aug = iaa.SigmoidContrast(gain=iap.Deterministic(gain), cutoff=iap.Deterministic(cutoff), per_channel=per_channel)
+            img_aug = aug.augment_image(img)
+            img3d_aug = aug.augment_image(img3d)
+            assert img_aug.dtype.type == np.uint8
+            assert img3d_aug.dtype.type == np.uint8
+            assert np.array_equal(img_aug, skimage.exposure.adjust_sigmoid(img, gain=gain, cutoff=cutoff))
+            assert np.array_equal(img3d_aug, skimage.exposure.adjust_sigmoid(img3d, gain=gain, cutoff=cutoff))
+
+    # check that tuple to uniform works
+    # note that gain and cutoff are saved in inverted order in _ContrastFuncWrapper to match
+    # the order of skimage's function
+    aug = iaa.SigmoidContrast(gain=(1, 2), cutoff=(0.25, 0.75))
+    assert isinstance(aug.params1d[0], iap.Uniform)
+    assert isinstance(aug.params1d[0].a, iap.Deterministic)
+    assert isinstance(aug.params1d[0].b, iap.Deterministic)
+    assert np.allclose(aug.params1d[0].a.value, 0.25)
+    assert np.allclose(aug.params1d[0].b.value, 0.75)
+    assert isinstance(aug.params1d[1], iap.Uniform)
+    assert isinstance(aug.params1d[1].a, iap.Deterministic)
+    assert isinstance(aug.params1d[1].b, iap.Deterministic)
+    assert aug.params1d[1].a.value == 1
+    assert aug.params1d[1].b.value == 2
+
+    # check that list to choice works
+    # note that gain and cutoff are saved in inverted order in _ContrastFuncWrapper to match
+    # the order of skimage's function
+    aug = iaa.SigmoidContrast(gain=[1, 2], cutoff=[0.25, 0.75])
+    assert isinstance(aug.params1d[0], iap.Choice)
+    assert all([np.allclose(val, val_choice) for val, val_choice in zip([0.25, 0.75], aug.params1d[0].a)])
+    assert isinstance(aug.params1d[1], iap.Choice)
+    assert all([val in aug.params1d[1].a for val in [1, 2]])
+
+    # check that per_channel at 50% prob works
+    aug = iaa.SigmoidContrast(gain=(1, 10), cutoff=(0.25, 0.75), per_channel=0.5)
+    seen = [False, False]
+    img1000d = np.zeros((1, 1, 1000), dtype=np.uint8) + 128
+    for _ in sm.xrange(100):
+        img_aug = aug.augment_image(img1000d)
+        assert img_aug.dtype.type == np.uint8
+        l = len(set(img_aug.flatten().tolist()))
+        if l == 1:
+            seen[0] = True
+        else:
+            seen[1] = True
+        if all(seen):
+            break
+    assert all(seen)
+
+    # check that keypoints are not changed
+    kpsoi = ia.KeypointsOnImage([ia.Keypoint(1, 1)], shape=(3, 3, 3))
+    kpsoi_aug = iaa.SigmoidContrast(gain=10, cutoff=0.5).augment_keypoints([kpsoi])
+    assert keypoints_equal([kpsoi], kpsoi_aug)
+
+    # check that heatmaps are not changed
+    heatmaps = ia.HeatmapsOnImage(np.zeros((3, 3, 1), dtype=np.float32) + 0.5, shape=(3, 3, 3))
+    heatmaps_aug = iaa.SigmoidContrast(gain=10, cutoff=0.5).augment_heatmaps([heatmaps])[0]
     assert np.allclose(heatmaps.arr_0to1, heatmaps_aug.arr_0to1)
 
 
