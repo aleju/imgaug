2fb7f8813a4979f56eb3201abf242ad4e943c928
aleju <kontakt@ajung.name>
2019-02-24 20:54:09 +0100

    Fix div by zero in angle_between_vectors()

--- imgaug/imgaug.py
+++ imgaug/imgaug.py
@@ -812,8 +812,10 @@ def angle_between_vectors(v1, v2):
     3.141592...
 
     """
-    v1_u = v1 / np.linalg.norm(v1)
-    v2_u = v2 / np.linalg.norm(v2)
+    l1 = np.linalg.norm(v1)
+    l2 = np.linalg.norm(v2)
+    v1_u = (v1 / l1) if l1 > 0 else np.float32(v1) * 0
+    v2_u = (v2 / l2) if l2 > 0 else np.float32(v2) * 0
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 
