bf41829f7d724a1d45aa43d5a020bb0fcd5d363e
aleju <kontakt@ajung.name>
2019-03-05 17:17:05 +0100

    Add polygon augmentation method to Fliplr

--- imgaug/augmenters/flip.py
+++ imgaug/augmenters/flip.py
@@ -120,6 +120,12 @@ class Fliplr(meta.Augmenter):  # pylint: disable=locally-disabled, unused-variab
                     keypoint.x = (width - 1) - keypoint.x
         return keypoints_on_images
 
+    def _augment_polygons(self, polygons_on_images, random_state, parents,
+                          hooks):
+        # TODO maybe reverse the order of points afterwards? the flip probably inverts them
+        return self._augment_polygons_as_keypoints(
+            polygons_on_images, random_state, parents, hooks)
+
     def get_parameters(self):
         return [self.p]
 
