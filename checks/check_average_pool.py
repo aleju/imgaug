41cccd227dc265ed317eb9a4445a9d715428e40d
aleju <kontakt@ajung.name>
2019-05-23 20:50:36 +0200

    Add MaxPool to check script

--- checks/check_average_pool.py
+++ checks/check_average_pool.py
@@ -7,19 +7,24 @@ import imgaug.augmenters as iaa
 
 
 def main():
+    run(iaa.AveragePool)
+    run(iaa.MaxPool)
+
+
+def run(clazz):
     image = ia.quokka_square((128, 128))
-    aug = iaa.AveragePool(2)
+    aug = clazz(2)
     ia.imshow(
         ia.draw_grid(aug.augment_images([image] * (5*5)))
     )
 
-    aug = iaa.AveragePool(2, keep_size=False)
+    aug = clazz(2, keep_size=False)
     ia.imshow(
         ia.draw_grid(aug.augment_images([image] * (5*5)))
     )
 
-    aug_pool = iaa.AveragePool(((0, 10), (0, 10)))
-    aug_blur = iaa.AverageBlur(((0, 10), (0, 10)))
+    aug_pool = clazz(((0, 10), (0, 10)))
+    aug_blur = clazz(((0, 10), (0, 10)))
     ia.imshow(
         np.hstack([
             ia.draw_grid(aug_pool.augment_images([image] * (4*5)), cols=4),
