50c1b7a5150b75e4ce0d1b07646afde1fc1574e3
aleju <kontakt@ajung.name>
2019-08-18 10:49:55 +0200

    Improve code style and docstrings of .augmentables.utils module

--- imgaug/augmentables/utils.py
+++ imgaug/augmentables/utils.py
@@ -6,8 +6,7 @@ import imgaug as ia
 
 # TODO integrate into keypoints
 def normalize_shape(shape):
-    """
-    Normalize a shape tuple or array to a shape tuple.
+    """Normalize a shape ``tuple`` or ``array`` to a shape ``tuple``.
 
     Parameters
     ----------
@@ -17,7 +16,7 @@ def normalize_shape(shape):
     Returns
     -------
     tuple of int
-        Shape tuple.
+        Shape ``tuple``.
 
     """
     if isinstance(shape, tuple):
@@ -29,17 +28,17 @@ def normalize_shape(shape):
 
 # TODO integrate into keypoints
 def project_coords(coords, from_shape, to_shape):
-    """
-    Project coordinates from one image shape to another.
+    """Project coordinates from one image shape to another.
 
-    This performs a relative projection, e.g. a point at 60% of the old
-    image width will be at 60% of the new image width after projection.
+    This performs a relative projection, e.g. a point at ``60%`` of the old
+    image width will be at ``60%`` of the new image width after projection.
 
     Parameters
     ----------
-    coords : ndarray or tuple of number
-        Coordinates to project. Either a ``(N,2)`` numpy array or a tuple
-        of `(x,y)` coordinates.
+    coords : ndarray or list of tuple of number
+        Coordinates to project.
+        Either an ``(N,2)`` numpy array or a ``list`` containing ``(x,y)``
+        coordinate ``tuple`` s.
 
     from_shape : tuple of int or ndarray
         Old image shape.
@@ -76,31 +75,113 @@ def project_coords(coords, from_shape, to_shape):
     return coords_proj
 
 
+# TODO does that include point_b in the result?
 def interpolate_point_pair(point_a, point_b, nb_steps):
+    """Interpolate ``N`` points on a line segment.
+
+    Parameters
+    ----------
+    point_a : iterable of number
+        Start point of the line segment, given as ``(x,y)`` coordinates.
+
+    point_b : iterable of number
+        End point of the line segment, given as ``(x,y)`` coordinates.
+
+    nb_steps : int
+        Number of points to interpolate between `point_a` and `point_b`.
+
+    Returns
+    -------
+    list of tuple of number
+        The interpolated points.
+        Does not include `point_a`.
+
+    """
     if nb_steps < 1:
         return []
     x1, y1 = point_a
     x2, y2 = point_b
     vec = np.float32([x2 - x1, y2 - y1])
     step_size = vec / (1 + nb_steps)
-    return [(x1 + (i + 1) * step_size[0], y1 + (i + 1) * step_size[1]) for i in sm.xrange(nb_steps)]
+    return [
+        (x1 + (i + 1) * step_size[0], y1 + (i + 1) * step_size[1])
+        for i
+        in sm.xrange(nb_steps)]
 
 
 def interpolate_points(points, nb_steps, closed=True):
+    """Interpolate ``N`` on each line segment in a line string.
+
+    Parameters
+    ----------
+    points : iterable of iterable of number
+        Points on the line segments, each one given as ``(x,y)`` coordinates.
+        They are assumed to form one connected line string.
+
+    nb_steps : int
+        Number of points to interpolate on each individual line string.
+
+    closed : bool, optional
+        If ``True`` the output contains the last point in `points`.
+        Otherwise it does not (but it will contain the interpolated points
+        leading to the last point).
+
+    Returns
+    -------
+    list of tuple of number
+        Coordinates of `points`, with additional `nb_steps` new points
+        interpolated between each point pair. If `closed` is ``False``,
+        the last point in `points` is not returned.
+
+    """
     if len(points) <= 1:
         return points
     if closed:
         points = list(points) + [points[0]]
     points_interp = []
     for point_a, point_b in zip(points[:-1], points[1:]):
-        points_interp.extend([point_a] + interpolate_point_pair(point_a, point_b, nb_steps))
+        points_interp.extend(
+            [point_a]
+            + interpolate_point_pair(point_a, point_b, nb_steps)
+        )
     if not closed:
         points_interp.append(points[-1])
-    # close does not have to be reverted here, as last point is not included in the extend()
+    # close does not have to be reverted here, as last point is not included
+    # in the extend()
     return points_interp
 
 
 def interpolate_points_by_max_distance(points, max_distance, closed=True):
+    """Interpolate points with distance ``d`` on a line string.
+
+    For a list of points ``A, B, C``, if the distance between ``A`` and ``B``
+    is greater than `max_distance`, it will place at least one point between
+    ``A`` and ``B`` at ``A + max_distance * (B - A)``. Multiple points can
+    be placed between the two points if they are far enough away from each
+    other. The process is repeated for ``B`` and ``C``.
+
+    Parameters
+    ----------
+    points : iterable of iterable of number
+        Points on the line segments, each one given as ``(x,y)`` coordinates.
+        They are assumed to form one connected line string.
+
+    max_distance : number
+        Maximum distance between any two points in the result.
+
+    closed : bool, optional
+        If ``True`` the output contains the last point in `points`.
+        Otherwise it does not (but it will contain the interpolated points
+        leading to the last point).
+
+    Returns
+    -------
+    list of tuple of number
+        Coordinates of `points`, with interpolated points added to the
+        iterable. If `closed` is ``False``, the last point in `points` is not
+        returned.
+
+    """
     assert max_distance > 0, (
         "Expected max_distance to have a value >0, got %.8f." % (
             max_distance,))
@@ -110,9 +191,13 @@ def interpolate_points_by_max_distance(points, max_distance, closed=True):
         points = list(points) + [points[0]]
     points_interp = []
     for point_a, point_b in zip(points[:-1], points[1:]):
-        dist = np.sqrt((point_a[0] - point_b[0]) ** 2 + (point_a[1] - point_b[1]) ** 2)
+        dist = np.sqrt(
+            (point_a[0] - point_b[0]) ** 2
+            + (point_a[1] - point_b[1]) ** 2)
         nb_steps = int((dist / max_distance) - 1)
-        points_interp.extend([point_a] + interpolate_point_pair(point_a, point_b, nb_steps))
+        points_interp.extend(
+            [point_a]
+            + interpolate_point_pair(point_a, point_b, nb_steps))
     if not closed:
         points_interp.append(points[-1])
     return points_interp
