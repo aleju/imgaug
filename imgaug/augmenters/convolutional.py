d57f8b30dc9d3f7129467dbbe19292bda802f1c1
aleju <kontakt@ajung.name>
2019-08-24 16:54:52 +0200

    Switch Emboss from function to class

--- imgaug/augmenters/convolutional.py
+++ imgaug/augmenters/convolutional.py
@@ -324,8 +324,7 @@ class Sharpen(Convolve):
             random_state=random_state)
 
 
-def Emboss(alpha=0, strength=1,
-           name=None, deterministic=False, random_state=None):
+class Emboss(Convolve):
     """
     Emboss images and alpha-blend the result with the original input images.
 
@@ -382,44 +381,43 @@ def Emboss(alpha=0, strength=1,
     using a random blending factor between ``0%`` and ``100%``.
 
     """
-    alpha_param = iap.handle_continuous_param(
-        alpha, "alpha",
-        value_range=(0, 1.0), tuple_to_uniform=True, list_to_choice=True)
-    strength_param = iap.handle_continuous_param(
-        strength, "strength",
-        value_range=(0, None), tuple_to_uniform=True, list_to_choice=True)
+    def __init__(self, alpha=0, strength=1,
+                 name=None, deterministic=False, random_state=None):
+        alpha_param = iap.handle_continuous_param(
+            alpha, "alpha",
+            value_range=(0, 1.0), tuple_to_uniform=True, list_to_choice=True)
+        strength_param = iap.handle_continuous_param(
+            strength, "strength",
+            value_range=(0, None), tuple_to_uniform=True, list_to_choice=True)
 
-    def create_matrices(image, nb_channels, random_state_func):
-        alpha_sample = alpha_param.draw_sample(random_state=random_state_func)
-        assert 0 <= alpha_sample <= 1.0, (
-            "Expected 'alpha' to be in the interval [0.0, 1.0], got %.4f." % (
-                alpha_sample))
-        strength_sample = strength_param.draw_sample(
-            random_state=random_state_func)
-        matrix_nochange = np.array([
-            [0, 0, 0],
-            [0, 1, 0],
-            [0, 0, 0]
-        ], dtype=np.float32)
-        matrix_effect = np.array([
-            [-1-strength_sample, 0-strength_sample, 0],
-            [0-strength_sample, 1, 0+strength_sample],
-            [0, 0+strength_sample, 1+strength_sample]
-        ], dtype=np.float32)
-        matrix = (
-            (1-alpha_sample) * matrix_nochange
-            + alpha_sample * matrix_effect
-        )
-        return [matrix] * nb_channels
+        def _create_matrices(_image, nb_channels, random_state_func):
+            alpha_sample = alpha_param.draw_sample(
+                random_state=random_state_func)
+            assert 0 <= alpha_sample <= 1.0, (
+                "Expected 'alpha' to be in the interval [0.0, 1.0], "
+                "got %.4f." % (
+                    alpha_sample))
+            strength_sample = strength_param.draw_sample(
+                random_state=random_state_func)
+            matrix_nochange = np.array([
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0]
+            ], dtype=np.float32)
+            matrix_effect = np.array([
+                [-1-strength_sample, 0-strength_sample, 0],
+                [0-strength_sample, 1, 0+strength_sample],
+                [0, 0+strength_sample, 1+strength_sample]
+            ], dtype=np.float32)
+            matrix = (
+                (1-alpha_sample) * matrix_nochange
+                + alpha_sample * matrix_effect
+            )
+            return [matrix] * nb_channels
 
-    if name is None:
-        name = "Unnamed%s" % (ia.caller_name(),)
-
-    return Convolve(
-        create_matrices,
-        name=name,
-        deterministic=deterministic,
-        random_state=random_state)
+        super(Emboss, self).__init__(
+            matrix=_create_matrices, name=name, deterministic=deterministic,
+            random_state=random_state)
 
 
 # TODO add tests
