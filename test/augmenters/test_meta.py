bc9d6268b145757e81cf6d7ef829237ff7890496
aleju <kontakt@ajung.name>
2019-09-21 21:44:54 +0200

    Add tests for unusual image axis sizes in WithChannels

--- test/augmenters/test_meta.py
+++ test/augmenters/test_meta.py
@@ -6571,6 +6571,46 @@ class TestWithChannels(unittest.TestCase):
             got_exception = True
         assert got_exception
 
+    def test_zero_sized_axes(self):
+        shapes = [
+            (0, 0),
+            (0, 1),
+            (1, 0),
+            (0, 1, 0),
+            (1, 0, 0),
+            (0, 1, 1),
+            (1, 0, 1)
+        ]
+
+        for shape in shapes:
+            with self.subTest(shape=shape):
+                image = np.zeros(shape, dtype=np.uint8)
+                aug = iaa.WithChannels([0], iaa.Add(1))
+
+                image_aug = aug(image=image)
+
+                assert image_aug.dtype.name == "uint8"
+                assert image_aug.shape == shape
+
+    def test_unusual_channel_numbers(self):
+        shapes = [
+            (1, 1, 4),
+            (1, 1, 5),
+            (1, 1, 512),
+            (1, 1, 513)
+        ]
+
+        for shape in shapes:
+            with self.subTest(shape=shape):
+                image = np.zeros(shape, dtype=np.uint8)
+                aug = iaa.WithChannels([0], iaa.Add(1))
+
+                image_aug = aug(image=image)
+
+                assert np.all(image_aug[..., 0] == 1)
+                assert image_aug.dtype.name == "uint8"
+                assert image_aug.shape == shape
+
     def test_get_parameters(self):
         aug = iaa.WithChannels([1], iaa.Add(10))
         params = aug.get_parameters()
