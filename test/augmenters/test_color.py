1d2eeef9adf2e267839d54e2b7e147feb93806a2
aleju <kontakt@ajung.name>
2019-09-21 20:36:46 +0200

    Add tests for unusual image axis sizes for quantize_colors_uniform()

--- test/augmenters/test_color.py
+++ test/augmenters/test_color.py
@@ -1801,3 +1801,41 @@ class Test_quantize_colors_uniform(unittest.TestCase):
             assert "[2..256]" in str(exc)
             got_exception = True
         assert got_exception
+
+    def test_zero_sized_axes(self):
+        shapes = [
+            (0, 0),
+            (0, 1),
+            (1, 0),
+            (0, 1, 0),
+            (1, 0, 0),
+            (0, 1, 1),
+            (1, 0, 1)
+        ]
+
+        for shape in shapes:
+            with self.subTest(shape=shape):
+                image = np.zeros(shape, dtype=np.uint8)
+
+                image_aug = iaa.quantize_colors_uniform(image, 2)
+
+                assert image_aug.dtype.name == "uint8"
+                assert image_aug.shape == shape
+
+    def test_unusual_channel_numbers(self):
+        shapes = [
+            (1, 1, 4),
+            (1, 1, 5),
+            (1, 1, 512),
+            (1, 1, 513)
+        ]
+
+        for shape in shapes:
+            with self.subTest(shape=shape):
+                image = np.zeros(shape, dtype=np.uint8)
+
+                image_aug = iaa.quantize_colors_uniform(image, 2)
+
+                assert np.any(image_aug > 0)
+                assert image_aug.dtype.name == "uint8"
+                assert image_aug.shape == shape
