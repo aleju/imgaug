cacb278857538d021481de570ee6aa57829cf788
aleju <kontakt@ajung.name>
2019-12-20 22:35:18 +0100

    Add Cutout augmenter
    This patch adds functionality to apply
    cutout augmentation, as described in the
    corresponding paper.
    
    The implementation of this patch uses a
    relative cutout size and position (rather
    than absolute one as in the paper's
    implementation). Furthermore, RGB fill
    values are supported (paper implementation
    always drops to mean value) and replacing
    cutout regions with gaussian noise
    (paper only supports constant values).
    
    * Add `imgaug.augmenters.arithmetic.cutout()`
    * Add `imgaug.augmenters.arithmetic.cutout_()`
    * Add `imgaug.augmenters.arithmetic.Cutout`
      augmenter

---
+++ checks/check_cutout.py
@@ -0,0 +1,15 @@
+from __future__ import print_function, division, absolute_import
+import imgaug as ia
+import imgaug.augmenters as iaa
+
+
+def main():
+    aug = iaa.Cutout(fill_mode=["gaussian", "constant"], cval=(0, 255),
+                     fill_per_channel=0.5)
+    image = ia.quokka()
+    images_aug = aug(images=[image] * 16)
+    ia.imshow(ia.draw_grid(images_aug, cols=4, rows=4))
+
+
+if __name__ == "__main__":
+    main()
