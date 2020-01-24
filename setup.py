9381d344172bb33f593d357106764004e4e60616
fchouteau <florient.chouteau@gmail.com>
2020-01-24 17:22:51 +0100

    Allow any opencv-* as requirement if already installed (#586)
    This patch changes `setup.py` to support any `opencv-*` package.
    I.e. if `opencv-python`, `opencv-python-headless`, `opencv-contrib-python`
    and `opencv-contrib-python-headless` are now all accepted and will no longer
    lead to another opencv package being installed.
    This is only the case if the library is installed using `python setup.py install`.

--- setup.py
+++ setup.py
@@ -1,8 +1,59 @@
+# pylint: disable=missing-module-docstring
+import re
+
+from pkg_resources import get_distribution, DistributionNotFound
 from setuptools import setup, find_packages
 
 long_description = """A library for image augmentation in machine learning experiments, particularly convolutional
 neural networks. Supports the augmentation of images, keypoints/landmarks, bounding boxes, heatmaps and segmentation
 maps in a variety of different ways."""
+
+INSTALL_REQUIRES = [
+    "six",
+    "numpy>=1.15",
+    "scipy",
+    "Pillow",
+    "matplotlib",
+    "scikit-image>=0.14.2",
+    "opencv-python-headless",
+    "imageio",
+    "Shapely",
+]
+
+ALT_INSTALL_REQUIRES = {
+    "opencv-python-headless": ["opencv-python", "opencv-contrib-python", "opencv-contrib-python-headless"],
+}
+
+
+def check_alternative_installation(install_require, alternative_install_requires):
+    """If some version version of alternative requirement installed, return alternative,
+    else return main.
+    """
+    for alternative_install_require in alternative_install_requires:
+        try:
+            alternative_pkg_name = re.split(r"[!<>=]", alternative_install_require)[0]
+            get_distribution(alternative_pkg_name)
+            return str(alternative_install_require)
+        except DistributionNotFound:
+            continue
+
+    return str(install_require)
+
+
+def get_install_requirements(main_requires, alternative_requires):
+    """Iterates over all install requires
+    If an install require has an alternative option, check if this option is installed
+    If that is the case, replace the install require by the alternative to not install dual package"""
+    install_requires = []
+    for main_require in main_requires:
+        if main_require in alternative_requires:
+            main_require = check_alternative_installation(main_require, alternative_requires.get(main_require))
+        install_requires.append(main_require)
+
+    return install_requires
+
+
+INSTALL_REQUIRES = get_install_requirements(INSTALL_REQUIRES, ALT_INSTALL_REQUIRES)
 
 setup(
     name="imgaug",
@@ -11,17 +62,7 @@ setup(
     author_email="kontakt@ajung.name",
     url="https://github.com/aleju/imgaug",
     download_url="https://github.com/aleju/imgaug/archive/0.3.0.tar.gz",
-    install_requires=[
-        "six",
-        "numpy>=1.15",
-        "scipy",
-        "Pillow",
-        "matplotlib",
-        "scikit-image>=0.14.2",
-        "opencv-python-headless",
-        "imageio",
-        "Shapely",
-    ],
+    install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     package_data={
