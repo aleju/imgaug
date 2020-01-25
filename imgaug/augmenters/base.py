79a4c48693d3525ae1e7cfbb587b95fb4325532d
aleju <kontakt@ajung.name>
2020-01-25 13:47:28 +0100

    Add SuspiciousMultiImageShapeWarning

--- imgaug/augmenters/base.py
+++ imgaug/augmenters/base.py
@@ -4,3 +4,7 @@ This module is planned to contain :class:`imgaug.augmenters.meta.Augmenter`
 in the future.
 
 """
+
+
+class SuspiciousMultiImageShapeWarning(UserWarning):
+    """Warning multi-image inputs that look like a single image."""
