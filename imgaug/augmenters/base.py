0eb6caf9080995b792e9cb4ca2d84a785b307acd
aleju <kontakt@ajung.name>
2020-01-25 13:47:57 +0100

    Add SuspiciousSingleImageShapeWarning

--- imgaug/augmenters/base.py
+++ imgaug/augmenters/base.py
@@ -8,3 +8,7 @@ in the future.
 
 class SuspiciousMultiImageShapeWarning(UserWarning):
     """Warning multi-image inputs that look like a single image."""
+
+
+class SuspiciousSingleImageShapeWarning(UserWarning):
+    """Warning for single-image inputs that look like multiple images."""
