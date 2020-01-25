1f68eb4959e8845c6da56d90f8e8b1fa173c4dce
aleju <kontakt@ajung.name>
2020-01-25 13:49:24 +0100

    Add _warn_on_suspicious_multi_image_shapes()

--- imgaug/augmenters/base.py
+++ imgaug/augmenters/base.py
@@ -4,6 +4,7 @@ This module is planned to contain :class:`imgaug.augmenters.meta.Augmenter`
 in the future.
 
 """
+import imgaug as ia
 
 
 class SuspiciousMultiImageShapeWarning(UserWarning):
@@ -12,3 +13,24 @@ class SuspiciousMultiImageShapeWarning(UserWarning):
 
 class SuspiciousSingleImageShapeWarning(UserWarning):
     """Warning for single-image inputs that look like multiple images."""
+
+
+def _warn_on_suspicious_multi_image_shapes(images):
+    if images is None:
+        return
+
+    # check if it looks like (H, W, C) instead of (N, H, W)
+    if ia.is_np_array(images):
+        if images.ndim == 3 and images.shape[-1] in [1, 3]:
+            ia.warn(
+                "You provided a numpy array of shape %s as a "
+                "multi-image augmentation input, which was interpreted as "
+                "(N, H, W). The last dimension however has value 1 or "
+                "3, which indicates that you provided a single image "
+                "with shape (H, W, C) instead. If that is the case, "
+                "you should use e.g. augmenter(image=<your input>) or "
+                "augment_image(<your input>) -- note the singular 'image' "
+                "instead of 'imageS'. Otherwise your single input image "
+                "will be interpreted as multiple images of shape (H, W) "
+                "during augmentation." % (images.shape,),
+                category=SuspiciousMultiImageShapeWarning)
