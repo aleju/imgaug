df083430b2ee20dffce39988c4e53205abd83063
aleju <kontakt@ajung.name>
2020-04-21 19:38:18 +0200

    Fix pylint issue

--- imgaug/imgaug.py
+++ imgaug/imgaug.py
@@ -2201,8 +2201,8 @@ def _median_pool_cv2(arr, block_size, pad_mode, pad_cval):
 
     ndim_in = arr.ndim
 
-    s = arr.shape
-    if s[0] % block_size != 0 or s[1] % block_size != 0:
+    shape = arr.shape
+    if shape[0] % block_size != 0 or shape[1] % block_size != 0:
         arr = pad_to_multiples_of(
             arr,
             height_multiple=block_size,
