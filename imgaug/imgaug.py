0b4b7e2828a5ecba362d1073ea8347eb2cd07d8e
aleju <kontakt@ajung.name>
2020-05-31 17:14:20 +0200

    Improve uint8 dtype check

--- imgaug/imgaug.py
+++ imgaug/imgaug.py
@@ -63,6 +63,9 @@ _POOLING_KERNELS_CACHE = {}
 # Added in 0.5.0.
 _NUMBA_INSTALLED = numba is not None
 
+# Added in 0.5.0.
+_UINT8_DTYPE = np.dtype("uint8")
+
 
 ###############################################################################
 # Helpers for deprecation
@@ -2555,10 +2558,8 @@ def apply_lut_(image, table):
 
         return np.concatenate(subluts, axis=2)
 
-    assert image.dtype.name == "uint8", (
+    assert image.dtype == _UINT8_DTYPE, (
         "Expected uint8 image, got dtype %s." % (image.dtype.name,))
-    assert table.dtype.name == "uint8", (
-        "Expected uint8 table, got dtype %s." % (table.dtype.name,))
 
     image = cv2.LUT(image, table, dst=image)
     return image
